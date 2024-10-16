8a18740ab9bb056d9f19ff027a495aa464da99f9
Mansi Alhat <mansialhat29@gmail.com>
2024-10-17 00:11:28 +0530

    Updated homepage and home structure, added CSS styles, and improved responsiveness

--- FarmerApp/post/urls.py
+++ FarmerApp/post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),  # This is the main homepage
+    path('home/', views.home, name='home'),      # This is the secondary home view
 ]
