5e5b47bf3fad4ef5721412422b5822e9b40d5fb2
Mansi Alhat <mansialhat29@gmail.com>
2024-10-30 21:55:18 +0530

    homepage changes

--- FarmerApp/post/views.py
+++ FarmerApp/post/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def homepage(request):
-    return render(request, 'post/homepage.html')
+def form(request):
+    return render(request, 'post/form.html')
 def home(request):
     return render(request, 'post/home.html')
